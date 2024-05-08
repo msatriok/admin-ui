cd53e70ad287661fd8f71c3d1210a093695133d8
msatriok <msatriokurniawan95@gmail.com>
2024-05-08 19:37:03 +0700

    ubah teks di app

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,62 @@
+import "./sidebar.scss"
+import { Link } from "react-router-dom";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import PersonIcon from '@mui/icons-material/Person';
+import ProductionQuantityLimitsIcon from '@mui/icons-material/ProductionQuantityLimits';
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import ExitToAppIcon from '@mui/icons-material/ExitToApp';
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+  <div className="top">
+    <Link to="/">
+      <span className="logo">Store</span>
+    </Link>
+  </div>
+  <hr />
+  <div className="center">
+    <ul>
+      <p className="title">MAIN</p>
+      <li>
+        <DashboardIcon className="icon"/>
+        <span>Dashboard</span>
+      </li>
+      <p className="title">LIST</p>
+      <Link to="/users">
+        <li>
+          <PersonIcon className="icon"/>
+          <span>Users</span>
+        </li>
+      </Link>
+      <Link to="/products">
+        <li>
+          <ProductionQuantityLimitsIcon className="icon"/>
+          <span>Products</span>
+        </li>
+      </Link>
+      <li>
+        <ShoppingBasketIcon className="icon"/>
+        <span>Orders</span>
+      </li>
+      <p className="title">USER</p>
+      <li>
+        <AccountBoxIcon className="icon"/>
+        <span>Profile</span>
+      </li>
+      <li>
+        <ExitToAppIcon className="icon"/>
+        <span>Logout</span>
+      </li>
+    </ul>
+  </div>
+  <div className="bottom">
+    <div className="colorOption"></div>
+    <div className="colorOption"></div>
+  </div>
+</div>
+  )
+}
+
+export default Sidebar
